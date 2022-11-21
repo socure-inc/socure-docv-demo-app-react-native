67fa51b65dc24e9d627ccb254aed7549386c26d2
shekhar-socure <107172234+shekhar-socure@users.noreply.github.com>
2022-11-21 20:32:48 +0530

    Sample app v2 - v1.2.0

--- v2/SampleApp/App.js
+++ v2/SampleApp/App.js
@@ -1,48 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Socure from 'rn-socure-sdk';
 
+import { StyleSheet, Button, View, Text, ToastAndroid, Alert, ActivityIndicator } from 'react-native';
 
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+const App = () => {
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+  const [isUploading, setUploading] = useState(false);
 
-const startLicense = () => {
-  Socure.scanLicense().then((res) => {
-    console.log('RES: ', res);
-    navigation.navigate('ScannedInformation', res);
-  });
-};
-
-
-const startPassport = () => {
-  Socure.scanPassport().then((res) => {
-    console.log('RES: ', res);
-    navigation.navigate('ScannedInformation', {
-      barcode: res.mrz,
-      type: res.type,
+  const startLicense = () => {
+    Socure.scanLicense().then((res) => {
+      console.log('RES: ', res);
+      notifyMessage("Captured License")
+    }, err => {
+      console.log("Error: ", err)
+      notifyMessage(err.message)
     });
-  });
-};
+  };
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-       Dcocument Verification in Just few Steps
-      </Text>
-      <Button
-        title="Take selfie"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
-    
-    <View>
-      <Text style={styles.title}>
-        
-      </Text>
-      <View style={styles.fixToText}>
+  const startPassport = () => {
+    Socure.scanPassport().then((res) => {
+      console.log('RES: ', res);
+      notifyMessage("Captured Passport")
+    }, err => {
+      console.log("Error: ", err)
+      notifyMessage(err.message)
+    });
+  };
+
+  const startSelfie = () => {
+    Socure.captureSelfie().then((res) => {
+      console.log('RES: ', res);
+      notifyMessage("Captured Selfie")
+    }, err => {
+      console.log("Error: ", err)
+      notifyMessage(err.message)
+    });
+  };
+
+  const setPublicKey = () => {
+    try {
+      Socure.setSocureSdkKey("YOUR_DOCV_SDK_KEY")
+      notifyMessage("Public key set")
+    } catch (ex) {
+      notifyMessage(ex)
+    }
+  };
+
+  const uploadScannedInfo = () => {
+    setUploading(true)
+    Socure.uploadScannedInfo().then((res) => {
+      setUploading(false)
+      console.log('RES: ', res);
+      notifyMessage("Upload Success")
+    }, err => {
+      setUploading(false)
+      console.log("Error: ", err)
+      notifyMessage(err.message)
+    });
+  };
+
+  const notifyMessage = (msg) => {
+    if (Platform.OS === 'android') {
+      ToastAndroid.show(msg, ToastAndroid.SHORT);
+    } else {
+      Alert.alert(msg);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Dcocument Verification in Just few Steps
+        </Text>
+      </View>
+
+      <View style={styles.row1}>
         <Button
           title="Scan Passport"
           onPress={startPassport}
@@ -51,10 +84,31 @@ const App = () => (
           title="Scan ID"
           onPress={startLicense}
         />
+        <Button
+          title="Take selfie"
+          onPress={startSelfie}
+        />
+      </View>
+
+      <View style={styles.row2}>
+        <Button
+          title="Set Public Key"
+          onPress={setPublicKey}
+          color="#841584"
+        />
+        <View style={styles.row3}>
+          {isUploading ? <ActivityIndicator /> : null}
+          <Button
+            disabled={isUploading}
+            title="Upload Scan"
+            onPress={uploadScannedInfo}
+            color="#ff8c1a"
+          />
+        </View>
       </View>
     </View>
-  </SafeAreaView>
-);
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -66,14 +120,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginVertical: 8,
   },
-  fixToText: {
+  row1: {
+    margin: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  row2: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  row3: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
