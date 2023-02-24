dc1875d4edeb28f796a75658353b910c55946f60
shekhar.gupta <shekhar.gupta@socure.me>
2023-02-24 12:13:05 +0530

    sample app v3.2.0

--- SampleApp/App.js
+++ SampleApp/App.js
@@ -16,7 +16,7 @@ import {
   Alert,
   Text,
 } from 'react-native';
-import {launchSocureDocV} from @socure-inc/docv-react-native';
+import {launchSocureDocV} from '@socure-inc/docv-react-native';
 
 export default function App() {
   const [status, setStatus] = useState<string>('');
